2ae11875886f5df905ee03e29db9c63a8925dbe7
babaroga <mama@mia.com>
2021-05-22 18:20:26 -0400

    init

---
+++ crunch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+
+namespace crunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ko = "koskos";
+            Console.WriteLine($"{ko}");
+
+
+        }
+    }
+
+    class DateTimeUtils
+    {
+
+    }
+}
