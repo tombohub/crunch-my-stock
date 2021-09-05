fd190c96308497adc649fb6b98cb59cd7222180f
tombohub <tkutlesa@gmail.com>
2021-09-05 11:56:59 -0400

    calculate end date

--- crunch/Strategies/Overnight/PriceDownloadOptions.cs
+++ crunch/Strategies/Overnight/PriceDownloadOptions.cs
@@ -4,24 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
+using Crunch.Core;
 
 namespace Crunch.Strategies.Overnight
 {
     class PriceDownloadOptions
     {
-        public DateTime Start { get; set; }
+        public DateTime Start { get; }
+        public DateTime End { get; }
+        public PriceInterval Interval { get; }
 
         public PriceDownloadOptions(int weekNum)
         {
             Start = CalculateStartDate(weekNum);
+            End = CalculateEndDate(weekNum);
+            Interval = PriceInterval.OneDay;
         }
 
         /// <summary>
         /// Calculate start date for Weekly Overnight strategy. Takes holidays into account.
         /// </summary>
         /// <param name="weekNum">calendar week number</param>
-        /// <returns>start dat</returns>
-        private DateTime CalculateStartDate(int weekNum)
+        /// <returns>start date</returns>
+        private static DateTime CalculateStartDate(int weekNum)
         {
             DateTime start = ISOWeek.ToDateTime(2021, weekNum, DayOfWeek.Monday);
             while (!TradingCalendar.IsTradingDay(start))
@@ -29,7 +34,23 @@ namespace Crunch.Strategies.Overnight
                 start = start.AddDays(1);
             }
             return start;
-            
         }
+
+        /// <summary>
+        /// Calculate end date for Weekly Overnight strategy. Takes holidays into account.
+        /// </summary>
+        /// <param name="weekNum">calendar week number</param>
+        /// <returns>end date</returns>
+        private static DateTime CalculateEndDate(int weekNum)
+        {
+            int nextWeekNum = weekNum + 1;
+            DateTime end = ISOWeek.ToDateTime(2021, nextWeekNum, DayOfWeek.Monday);
+            while (!TradingCalendar.IsTradingDay(end))
+            {
+                end = end.AddDays(1);
+            }
+            return end;
+        }
+
     }
 }
