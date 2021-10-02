ea5b56a5da4b7af725984564b7cd96c1a766d25f
tombohub <tkutlesa@gmail.com>
2021-10-02 13:50:46 -0400

    overnight reports test using common data in property

--- Tests/OvernightReportsTests.cs
+++ Tests/OvernightReportsTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Crunch.Strategies.Overnight;
 using Crunch.Database;
+using Crunch.Database.Models;
 using Crunch.Domain;
 
 
@@ -14,11 +15,18 @@ namespace CrunchTests
     [TestClass]
     public class OvernightReportsTests
     {
+        public static List<WeeklyOvernightStat> Stats { get; set; }
+
+        [ClassInitialize]
+        public static void GetStats(TestContext context)
+        {
+            Stats = DatabaseAPI.GetWeeklyOvernightStats(37);
+        }
         [TestMethod]
         public void GetSpyOvernightRoi_OvernightStatsData_ReturnsCorrectNumber()
         {
-            var stats = DatabaseAPI.GetWeeklyOvernightStats(37);
-            var overnightStats = new OvernightStats(stats);
+            //var stats = DatabaseAPI.GetWeeklyOvernightStats(37);
+            var overnightStats = new OvernightStats(Stats);
             var spyOvernightRoi = overnightStats.GetSpyOvernightRoi();
             Assert.AreEqual(spyOvernightRoi, -0.016582899999999946);
         }
@@ -26,8 +34,8 @@ namespace CrunchTests
         [TestMethod]
         public void GetSpyBenchmarkRoi_OvernightStatsData_ReturnsCorrectNumber()
         {
-            var stats = DatabaseAPI.GetWeeklyOvernightStats(37);
-            var overnightStats = new OvernightStats(stats);
+            //var stats = DatabaseAPI.GetWeeklyOvernightStats(37);
+            var overnightStats = new OvernightStats(Stats);
             var spyBenchmarkRoi = overnightStats.GetSpyBenchmarkRoi();
             Assert.AreEqual(spyBenchmarkRoi, -0.026090999999999975);
         }
@@ -35,8 +43,8 @@ namespace CrunchTests
         [TestMethod]
         public void CalculateAverageOvernightRoi_OvernightStocksStatsData_ReturnsCorrectNumber()
         {
-            var stats = DatabaseAPI.GetWeeklyOvernightStats(37);
-            var overnightStats = new OvernightStats(stats);
+            //var stats = DatabaseAPI.GetWeeklyOvernightStats(37);
+            var overnightStats = new OvernightStats(Stats);
             var avgRoi = overnightStats.CalculateAverageOvernightRoi(SecurityType.Stock);
             Assert.AreEqual(avgRoi, -0.017692364756611095);
         }
@@ -44,8 +52,8 @@ namespace CrunchTests
         [TestMethod]
         public void CalculateAverageOvernightRoi_OvernightEtfsStatsData_ReturnsCorrectNumber()
         {
-            var stats = DatabaseAPI.GetWeeklyOvernightStats(37);
-            var overnightStats = new OvernightStats(stats);
+            //var stats = DatabaseAPI.GetWeeklyOvernightStats(37);
+            var overnightStats = new OvernightStats(Stats);
             var avgRoi = overnightStats.CalculateAverageOvernightRoi(SecurityType.Etf);
             Assert.AreEqual(avgRoi, -0.016191809945275748);
         }
