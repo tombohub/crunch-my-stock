93df76b29be35945900fbd66e12685979411906b
tombohub <tkutlesa@gmail.com>
2021-10-03 10:09:32 -0400

    created overnight stats repositor

---
+++ crunch/Database/OvernightStatsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crunch.Strategies.Overnight;
+using Crunch.Database.Models;
+
+namespace Crunch.Database
+{
+    class OvernightStatsRepository: IOvernightStatsRepository
+    {
+        public OvernightStats GetOvernightStats(int weekNum)
+        {
+            var db = new stock_analyticsContext();
+            List<WeeklyOvernightStat> stats = db.WeeklyOvernightStats
+                .Where(s => s.WeekNum == weekNum).ToList();
+            db.Dispose();
+            var overnightStats = new OvernightStats(stats);
+
+            return overnightStats;
+        }
+    }
+}
