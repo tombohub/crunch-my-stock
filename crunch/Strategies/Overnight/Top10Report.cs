7aed5ac318b65d0c3ce9d8c81259eb8634a7548c
tombohub <tkutlesa@gmail.com>
2021-10-04 18:53:01 -0400

    modifications

--- crunch/Strategies/Overnight/Top10Report.cs
+++ crunch/Strategies/Overnight/Top10Report.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Crunch.Strategies.Overnight
 {
-    public class Top10Report
+    public record Top10Report
     {
         public string Symbol { get; init; }
         public double StrategyRoi { get; init; }
