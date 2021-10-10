b6b3348f779107e9002a29560478b59c8d5453d9
tombohub <tkutlesa@gmail.com>
2021-10-09 20:15:34 -0400

    multiplot layout, class change name

---
+++ crunch/Strategies/Overnight/Plots/MultiplotLayout.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crunch.Strategies.Overnight.Plots
+{
+    /// <summary>
+    /// Stup a grid layout for composite plotting.
+    /// Composite plotting is drawing single plots into one image - multiplot.
+    /// </summary>
+    class MultiplotLayout
+    {
+        /// <summary>
+        /// Numbr of rows and columns
+        /// </summary>
+        private Tuple<int, int> _gridTemplate;
+
+        /// <summary>
+        /// Size of the Multiplot in pixels
+        /// </summary>
+        private Tuple<int, int> Size { get; set; }
+    }
+}
