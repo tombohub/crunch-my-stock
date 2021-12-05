93526250bed168bf9aa4752cc9233f9a93a4fa6d
me <tkutlesa@gmail.com>
2021-12-05 11:58:45 -0500

    added

---
+++ crunch/Strategies/Overnight/ReportsCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crunch.Database.Models;
+using Crunch.Domain;
+using Crunch.Strategies.Overnight.Reports;
+
+namespace Crunch.Strategies.Overnight
+{
+    public class ReportsCollection
+    {
+        public WinnersLosersRatioReport WinnersLosersRatio { get; init; }
+        public List<Top10Report> Top10 { get; init; }
+        public List<Bottom10Report> Bottom10 { get; init; }
+        public double AverageOvernightRoi { get; init; }
+        public double AverageBenchmarkRoi { get; init; }
+        public double SpyOvernightRoi { get; init; }
+        public double SpyBenchmarkRoi { get; init; }
+      
+    }
+       
+}
