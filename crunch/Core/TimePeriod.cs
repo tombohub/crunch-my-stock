8b383b4d08a96c42238a8d8c56742f443096a721
me <tkutlesa@gmail.com>
2023-04-09 11:21:41 -0400

    time period value object

---
+++ crunch/Core/TimePeriod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Crunch.Core
+{
+    /// <summary>
+    /// Time period
+    /// </summary>
+    internal record TimePeriod
+    {
+        /// <summary>
+        /// Period start date
+        /// </summary>
+        public DateOnly Start { get; }
+
+        /// <summary>
+        /// Period end date
+        /// </summary>
+        public DateOnly End { get; }
+
+        public TimePeriod(DateOnly start, DateOnly end)
+        {
+            if (start > end)
+            {
+                throw new ArgumentException("Start date cannot be later than end date");
+            }
+            Start = start;
+            End = end;
+        }
+    }
+}
