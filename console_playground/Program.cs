8ac801ec066b7b097c14384ba0a69f9f18dee769
me <tkutlesa@gmail.com>
2023-04-09 11:21:28 -0400

    project for testing code

---
+++ console_playground/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace console_playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = new TimePeriod(new DateOnly(2022, 3, 6), new DateOnly(2022, 3, 5));
+            Console.WriteLine(k);
+        }
+    }
+
+    internal record TimePeriod
+    {
+        public DateOnly Start { get; init; }
+        public DateOnly End { get; }
+
+        public TimePeriod(DateOnly start, DateOnly end)
+        {
+            if (start > end)
+            {
+                throw new ArgumentException("Start date cannot be later than end date");
+            }
+        }
+    }
+}
