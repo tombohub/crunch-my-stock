2b4ad3b57b5fe341a9de6ad20b244f701bf8b68e
me <tkutlesa@gmail.com>
2023-05-16 10:52:24 -0400

    updated fkn nugets

---
+++ Tests/OvernightPricesTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrunchTests
+{
+    [TestClass]
+    internal class OvernightPricesTest
+    {
+        [TestMethod]
+        public void TransformToOvernightPricesTest_ReturnsEqual()
+        {
+
+        }
+    }
+}
