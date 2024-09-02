dfa322b56b89d0e0c5d14e0aecb7ebecf05bba04
me <tkutlesa@gmail.com>
2024-09-02 15:28:28 -0400

    use env vars

--- crunch/scaffold_models.py
+++ crunch/scaffold_models.py
@@ -3,10 +3,13 @@
 import os
 
 # configuration
-database_name = 'stock_analytics'
-models_dir = 'Database/Models/'
-context_name = 'stock_analyticsContext'
-context_file = context_name + '.cs'
+db_name = os.getenv("DB_NAME")
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+models_dir = "Database/Models/"
+context_name = "stock_analyticsContext"
+context_file = context_name + ".cs"
 context_dir = models_dir
 
 
@@ -19,34 +22,43 @@ for file in os.listdir(models_dir):
 try:
     os.remove(context_file)
 except Exception:
-    print('no context file present')
+    print("no context file present")
 
-#NOTE: these tables are from another project
-tables_to_scaffold = ['prices', 'stores', 'tools', 'sources', 'tool_types', 'brands', 'prices_ranked']
+# NOTE: these tables are from another project
+tables_to_scaffold = [
+    "prices",
+    "stores",
+    "tools",
+    "sources",
+    "tool_types",
+    "brands",
+    "prices_ranked",
+]
 
 # these need to have space at the end of each part to create full command
-base_command = 'dotnet ef dbcontext scaffold '
-db_connection_string = f'"Server=***REMOVED***;Port=5432;Database={database_name};User Id=***REMOVED***;Password=***REMOVED***;" '
-nuget_ef_core_provider = 'Npgsql.EntityFrameworkCore.PostgreSQL '
-models_output_dir = f'--output-dir {models_dir} '
-force_update_flag = '--force '
+base_command = "dotnet ef dbcontext scaffold "
+db_connection_string = f'"Server={db_host};Port=5432;Database={db_name};User Id={db_user};Password={db_password};" '
+nuget_ef_core_provider = "Npgsql.EntityFrameworkCore.PostgreSQL "
+models_output_dir = f"--output-dir {models_dir} "
+force_update_flag = "--force "
 
 # exclude because we want all tables to scaffold
 # tables = ' '.join(['--table ' + table for table in tables_to_scaffold])
-context_name = f'--context {context_name} '
-context_output_folder = f'--context-dir {context_dir} '
-no_build_flag = '--no-build '
+context_name = f"--context {context_name} "
+context_output_folder = f"--context-dir {context_dir} "
+no_build_flag = "--no-build "
 
 # keep tables last because there is no empty space after the last table
-full_command = ( base_command +
-                db_connection_string +
-                nuget_ef_core_provider +
-                context_output_folder +
-                models_output_dir +
-                context_name +
-                force_update_flag +
-                no_build_flag
+full_command = (
+    base_command
+    + db_connection_string
+    + nuget_ef_core_provider
+    + context_output_folder
+    + models_output_dir
+    + context_name
+    + force_update_flag
+    + no_build_flag
 )
 
-print(f'running command: {full_command}')
+print(f"running command: {full_command}")
 os.system(full_command)
